fdd6f52ff201bead2e06952fc7a183760f7fed8b
Sergiy Lebedyn <lebedyncrs@yandex.ru>
2016-04-19 21:36:37 +0200

    initial commit

---
+++ AttrsWatcherBehavior.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace industi\yii2\crm\components\behaviors;
+
+use yii\base\Behavior;
+use yii\db\BaseActiveRecord;
+use yii\base\InvalidConfigException;
+
+/**
+ * 
+ */
+class AttrsWatcherBehavior extends Behavior {
+
+    const FROM = 'from';
+    const TO = 'to';
+    const ATTRIBUTE = 'attribute';
+
+    /**
+     * @var array
+     */
+    public $attributes;
+
+    /**
+     * @var mixed 
+     */
+    public $values;
+
+    /**
+     * @inheritdoc
+     */
+    public function events() {
+        return [
+            BaseActiveRecord::EVENT_AFTER_VALIDATE => 'setAttribute',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function init() {
+        parent::init();
+        if (is_null($this->values)) {
+            $this->values = time();
+        }
+    }
+
+    public function setAttribute($event) {
+        foreach ($this->attributes as $key => $value) {
+            if (is_string($key) && $this->owner->hasAttribute($key)) {
+                $attrName = $key;
+            } elseif (is_string($value) && $this->owner->hasAttribute($key)) {
+                $attrName = $value;
+            }
+            if (!isset($attrName)) {
+                throw new InvalidConfigException("Attribute dosen't exist");
+            }
+            if ($this->isAttrChanged($attrName)) {
+                $this->owner->{$this->attributes[$attrName][self::ATTRIBUTE]} = $this->getValue($this->attributes[$attrName][self::ATTRIBUTE]);
+            }
+        }
+    }
+
+    /**
+     * check if attr is changed  
+     * @param string $attrName
+     * @return boolean
+     */
+    public function isAttrChanged($attrName) {
+        if ($this->owner->isAttributeChanged($attrName)) {
+            $fromExsistence = isset($this->attributes[$attrName][self::FROM]);
+            $toExsistence = isset($this->attributes[$attrName][self::TO]);
+            if ($fromExsistence && $toExsistence) {
+                if ($this->attributes[$attrName][self::FROM] === $this->owner->getOldAttribute($attrName) && $this->owner->{$attrName} === $this->attributes[$attrName][self::TO]) {
+                    return true;
+                }
+                return false;
+            } elseif ($fromExsistence) {
+                if ($this->attributes[$attrName][self::FROM] == $this->owner->getOldAttribute($attrName)) {
+                    return true;
+                }
+                return false;
+            } elseif ($toExsistence) {
+                if ($this->attributes[$attrName][self::TO] === $this->owner->{$attrName}) {
+                    return true;
+                }
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param string $attrName
+     * @return mixed
+     */
+    public function getValue($attrName) {
+        if (is_array($this->values) && isset($this->values[$attrName])) {
+            return $this->values[$attrName];
+        }
+        return time();
+    }
+
+}
